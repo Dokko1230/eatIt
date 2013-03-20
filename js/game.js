bb3f95b62f310c01fec638c14e9d0ec031b05783
Sean <dokko1230@gmail.com>
2013-03-20 13:54:27 -0700

    first commit'

---
+++ js/game.js
@@ -0,0 +1,175 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "images/background.png";
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+var monstersCaught = 0;
+var top_score = 0;
+var Timer;
+var TotalSeconds;
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+// Draw everything
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Top score: " + top_score, 32, 64);
+	ctx.fillText("Time: " + Timer, canvas.width - 256, 32);
+};
+
+function resetGame() {
+	TotalSeconds = 30;
+	monstersCaught = 0;
+}
+
+function Tick() {
+	if(TotalSeconds <= 0) {
+		if(top_score < monstersCaught){
+			top_score = monstersCaught;
+		}
+		resetGame();
+	}
+
+	TotalSeconds -= 1;
+	UpdateTimer();
+	window.setTimeout("Tick()", 1000);
+}
+
+function UpdateTimer() {
+	var Seconds = TotalSeconds;
+
+	var Days = Math.floor(Seconds / 86400);
+	Seconds -= Days * 86400;
+
+	var Hours = Math.floor(Seconds / 3600);
+	Seconds -= Hours * (3600);
+
+	var Minutes = Math.floor(Seconds / 60);
+	Seconds -= Minutes * (60);
+
+	var TimeStr = ((Days > 0) ? Days + " days " : "") + LeadingZero(Hours) + ":" + LeadingZero(Minutes) + ":" + LeadingZero(Seconds)
+
+
+	Timer = TimeStr;
+}
+
+function CreateTimer(Time) {
+	TotalSeconds = Time;
+	UpdateTimer();
+	window.setTimeout("Tick()", 1000);
+}
+
+function LeadingZero(Time) {
+	return (Time < 10) ? "0" + Time : + Time;
+}
+
+
+// The main game loop
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+	// console.log("main being called");
+	then = now;
+};
+
+// Let's play this game!
+reset();
+var then = Date.now();
+setInterval(main, 1); // Execute as fast as possible
+CreateTimer(30);
